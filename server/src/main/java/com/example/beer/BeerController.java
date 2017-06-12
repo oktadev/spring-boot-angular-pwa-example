1f2839f76379a0f505a32b04dc123942100d3615
Matt Raible <matt@raibledesigns.com>
2017-06-12 16:40:40 -0600

    Simplify BeerController

--- server/src/main/java/com/example/beer/BeerController.java
+++ server/src/main/java/com/example/beer/BeerController.java
@@ -5,8 +5,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Collection;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.stream.Collectors;
 
 @RestController
@@ -19,15 +17,11 @@ public class BeerController {
 
     @GetMapping("/good-beers")
     @CrossOrigin(origins = "http://localhost:4200")
-    public Collection<Map<String, String>> goodBeers() {
+    public Collection<Beer> goodBeers() {
 
         return repository.findAll().stream()
                 .filter(this::isGreat)
-                .map(b -> {
-                    Map<String, String> m = new HashMap<>();
-                    m.put("name", b.getName());
-                    return m;
-                }).collect(Collectors.toList());
+                .collect(Collectors.toList());
     }
 
     private boolean isGreat(Beer beer) {
