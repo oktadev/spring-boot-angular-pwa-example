9f66e629d7249465ca6ddf310ea66d2910648520
Matt Raible <matt.raible@okta.com>
2017-11-03 16:00:55 -0600

    Integrate Angular SDK

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -4,7 +4,23 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { BeerListComponent } from './beer-list/beer-list.component';
 import { HttpClientModule } from '@angular/common/http';
-import { MatListModule, MatToolbarModule, } from '@angular/material';
+import { MatListModule, MatToolbarModule } from '@angular/material';
+
+import { OktaAuthModule, OktaCallbackComponent } from '@okta/okta-angular';
+import { RouterModule, Routes } from '@angular/router';
+
+const appRoutes: Routes = [
+  {
+    path: 'implicit/callback',
+    component: OktaCallbackComponent
+  }
+];
+
+const config = {
+  issuer: 'https://dev-158606.oktapreview.com/oauth2/default',
+  redirectUri: 'http://localhost:4200/implicit/callback',
+  clientId: '0oac85oh5p2VqZJ7c0h7'
+};
 
 @NgModule({
   declarations: [
@@ -14,9 +30,12 @@ import { MatListModule, MatToolbarModule, } from '@angular/material';
   imports: [
     BrowserModule,
     HttpClientModule,
-    MatListModule, MatToolbarModule
+    MatListModule, MatToolbarModule,
+    RouterModule.forRoot(appRoutes),
+    OktaAuthModule.initAuth(config)
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
--- client/src/app/shared/beer/beer.service.ts
+++ client/src/app/shared/beer/beer.service.ts
@@ -1,13 +1,24 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs/Observable';
+import { Http, RequestOptions, Response, Headers } from '@angular/http';
+import 'rxjs/add/operator/map';
+import { Observable } from 'rxjs';
+import { OktaAuthService } from '@okta/okta-angular';
 
 @Injectable()
 export class BeerService {
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: Http, private oktaAuth: OktaAuthService) {
+  }
 
   getAll(): Observable<any> {
-    return this.http.get('http://localhost:8080/good-beers');
+    const headers: Headers = new Headers();
+    if (this.oktaAuth.isAuthenticated()) {
+      const accessToken = this.oktaAuth.getAccessToken().accessToken;
+      headers.append('Authorization', accessToken.tokenType + ' ' + accessToken.accessToken);
+    }
+    const options = new RequestOptions({headers: headers});
+
+    return this.http.get('http://localhost:8080/good-beers', options)
+      .map((response: Response) => response.json());
   }
 }
